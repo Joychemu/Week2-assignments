8f32383a824f605937d2d621bb3d48094befa484
Joychemu <64799702+Joychemu@users.noreply.github.com>
2025-03-11 16:29:31 +0300

    Update assignment1.py
    Complete assignment1

--- assignment1.py
+++ assignment1.py
@@ -2,5 +2,11 @@ num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 
 result = num1 + num2
+result1 = num1 - num2
+result2 = num1 * num2
+result3 = num1 / num2
 
 print(result)
+print(result1)
+print(result2)
+print(result3)
